d8a5f0c49c25df845cd3aa58fc1be4529a9cddc2
aenelle <aenelle@gmail.com>
2018-07-19 19:05:29 +0300

    fix

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+//Кому отправляем?
+$config['mail'] = 'call@medkvadrat.ru';
+
+//Использовать SMTP?
+$config['use_smtp'] = false;
+
+
+//От имени какого ящика отправляем?
+$config['from'] = 'info@artroz-kolena.ru';
+
+//Данные для SMTP отпарвки
+/*$config['smtp']['host'] = 'mail.weltkind.com';
+$config['smtp']['auth'] = 'login';
+$config['smtp']['username'] = 'noreply@weltkind.com';
+$config['smtp']['password'] = 'noreply';
+$config['smtp']['port'] = '25';*/
